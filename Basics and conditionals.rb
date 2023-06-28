0652dd322265d37bd4c1ca603afa368685877f6c
Glitch-guy0 <prajwalm9845@gmail.com>
2023-06-28 23:20:11 +0530

    conditional statements
    if else and nested if statements
    'basics' renamed to 'basics and conditionals'

---
+++ Basics and conditionals.rb
@@ -0,0 +1,56 @@
+# This is a single line commnet
+
+=begin
+This is a multiline commnet
+and it works like This
+=end
+
+#printing output
+print "using print\n"
+puts "using puts"
+
+# getting user input
+#gets 
+
+# Variables
+# This is similar to python it's dynamic
+
+myNumber = 34
+myString = 'this is a string'
+anotherString = "This is also a string"
+
+# taking input from the user
+
+puts "enter your value here: "
+input = gets
+
+# Conditional Statement
+
+# if else
+
+num = 5
+
+if num == 5
+    print "value of num is 5\n"
+else
+    print "value is not 5\n"
+end
+
+# nested if else
+
+numa = 5
+numb = 6
+if num == 6
+    print "this is 5"
+
+elsif numa == 5 && numb == 6
+    print "numa is 5 and numb is 6"
+else
+    print 'something'
+end
+print "\n"
+
+if num == 5
+    print "this is only if condition for reference"
+end
+print "\n"
